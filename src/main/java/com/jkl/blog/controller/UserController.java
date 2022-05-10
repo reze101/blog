ec954827965fd04a111c34db6ad7aebc3387bea9
reze101 <reze101@naver.com>
2022-05-10 17:10:20 +0900

    [2022-05-10] security login finished, kakao login ongoing

--- src/main/java/com/jkl/blog/controller/UserController.java
+++ src/main/java/com/jkl/blog/controller/UserController.java
@@ -1,7 +1,21 @@
 package com.jkl.blog.controller;
 
+import java.net.http.HttpHeaders;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jkl.blog.model.OAuthToken;
 
 @Controller
 public class UserController {
@@ -24,5 +38,52 @@ public class UserController {
 	public String updateForm() {
 		
 		return "user/updateForm";
+	}
+	
+	@GetMapping("/auth/kakao/callback")
+	public @ResponseBody String kakaoCallback(String code) {	//@ResponseBody : Data를 리턴해주는 컨트롤러 함수가 됨
+		
+		//인증토큰 발급을 위해 post 방식으로 key=value 형식의 데이터를 카카오쪽으로 요청해야 함
+		//http 요청
+		RestTemplate rt = new RestTemplate();	
+		
+		//http헤더 생성
+		org.springframework.http.HttpHeaders headers = new org.springframework.http.HttpHeaders();
+		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+		
+		//body에 값 담을 object 생성
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", "authorization_code");
+		params.add("client_id", "60af53dd62335119b0e681ffbcc766ee");
+		params.add("redirect_uri", "http://localhost:8000/auth/kakao/callback");
+		params.add("code", code);
+		
+		//body와 header를 담고있는 객체 생성 
+		HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = 
+				new HttpEntity<>(params, headers);
+		
+		//요청
+		ResponseEntity<String> response = rt.exchange(
+				"https://kauth.kakao.com/oauth/token", 
+				HttpMethod.POST,
+				kakaoTokenRequest,
+				String.class		//응답 받을 타입 
+				);
+		
+		//Gson, Json Simple, ObjectMapper : json data를 java object 로 처리하기 위함 
+		ObjectMapper objectMapper = new ObjectMapper();
+		OAuthToken oauthToken = null;
+		try {
+			oauthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("카카오엑세스 토큰 : "+oauthToken.getAccess_token());
+		return response.getBody();		
+
 	}
 }
--- src/main/webapp/WEB-INF/views/user/loginForm.jsp
+++ src/main/webapp/WEB-INF/views/user/loginForm.jsp
@@ -10,18 +10,22 @@
 		<div class="form-group">
 			<label for="password">Password:</label> <input type="password" class="form-control" placeholder="Enter password" id="password" name="password">
 		</div>
-<!--
+		<!--
 		<div class="form-group form-check">
 			<label class="form-check-label"> <input class="form-check-input" type="checkbox" name="remember"> Remember me
 			</label>
 		</div>
--->	
-	<button id="btn-login" class="btn btn-primary">로그인</button>
+-->
+		<button id="btn-login" class="btn btn-primary">로그인</button>
+		<a href="https://kauth.kakao.com/oauth/authorize?client_id=60af53dd62335119b0e681ffbcc766ee&redirect_uri=http://localhost:8000/auth/kakao/callback&response_type=code"> <img
+			src="/image/kakao_login_button.png" height="38px" />
+		</a>
 	</form>
 
 </div>
 
-<script type="text/javascript"  src="/js/user.js">
+<script type="text/javascript" src="/js/user.js">
+	
 </script>
 <%@ include file="../layout/footer.jsp"%>
 
